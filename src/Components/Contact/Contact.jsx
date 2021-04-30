da2da6523fac3cb4668b13a05fafc7836fe996a7
Grach90 <grach.tver777@gmail.com>
2021-04-30 10:46:00 +0300

    'main'

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,12 +1,12 @@
 // import ContactForm from "./ContactForm";
-import style from "./contact.module.css";
+// import style from "./contact.module.css";
 // import ContactFormProvider from '../../Context/Providers/ContactFormProvider';
 // import ContactFormWithContext from './ContactFormWithContext';
 import ContactFormWithRedux from './ContactFormWithRedux';
 
 function Contact(props){
   return (
-    <div className={style.body}>
+    <div >
       <h1>Contact Form</h1>
       <ContactFormWithRedux />
       {/* <ContactFormProvider>
